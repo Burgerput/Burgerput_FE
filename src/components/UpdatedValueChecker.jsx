36aa94f540a67bd7d1562501ec3089e9bb6deb6f
GoJiMin <gojimin3095@gmail.com>
2024-05-09 23:58:39 +0900

    feat: diffValue 강조 표시 기능 구현
    - 페이지에 진입했을 때 달라진 값이 존재한다면 3초 동안 해당 영역을 강조 표시합니다.

--- src/components/UpdatedValueChecker.jsx
+++ src/components/UpdatedValueChecker.jsx
@@ -3,28 +3,41 @@ import { getDiffValue } from "../api/Products";
 import styles from "./UpdatedValueChecker.module.css";
 import EditItem from "./ui/diffValues/EditItem";
 import AddedOrRemovedItem from "./ui/diffValues/AddedOrRemovedItem";
+import { useQuery } from "@tanstack/react-query";
 
 export default function UpdatedValueChecker() {
-  const [diffValue, setDiffValue] = useState();
+  const [showOverlay, setShowOverlay] = useState(true);
+
+  const { data: diffValue } = useQuery(["diffValue"], () => getDiffValue());
 
   useEffect(() => {
-    getDiffValue().then((data) => setDiffValue(data));
+    const timer = setTimeout(() => {
+      setShowOverlay(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
   }, []);
 
   return (
-    <section className={styles.section}>
-      <h2 className={styles.title}>👀 확인해주세요 👀</h2>
-      <ul className={styles.list}>
-        {diffValue &&
-          diffValue.map((item) => {
-            const Component =
-              item.code === "edit" ? EditItem : AddedOrRemovedItem;
-            const props =
-              item.code === "edit" ? { item } : { item, type: item.code };
+    <>
+      {diffValue && (
+        <>
+          {showOverlay && <div className={styles.overlay} />}
+          <section className={styles.section}>
+            <h2 className={styles.title}>👀 확인해주세요 👀</h2>
+            <ul className={styles.list}>
+              {diffValue.map((item) => {
+                const Component =
+                  item.code === "edit" ? EditItem : AddedOrRemovedItem;
+                const props =
+                  item.code === "edit" ? { item } : { item, type: item.code };
 
-            return <Component key={item.id} {...props} />;
-          })}
-      </ul>
-    </section>
+                return <Component key={item.id} {...props} />;
+              })}
+            </ul>
+          </section>
+        </>
+      )}
+    </>
   );
 }
