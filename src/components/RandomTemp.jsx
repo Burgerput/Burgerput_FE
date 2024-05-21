1db6f7fc7648f694b8029a04e44b0fd2447ce593
GoJiMin <gojimin3095@gmail.com>
2024-05-22 00:16:21 +0900

    feat: 슬라이더와 defaultValues 동기화 및 useWatch를 이용한 temp 값 표시
    - Controller의 name에 products의 idx에 해당하는 temp를 연결해 defaultValues를 동기화했습니다. 이를 통해 submit 이벤트 발생시 변경된 min, max 값을 받아옵니다.
    - useWatch를 이용해 slider를 이용해 변경되는 min, max 값을 화면에 표시합니다. 이 때 watch를 사용하지 않고 useWatch를 사용하는 이유는 단일 항목에 대한 리렌더링만을 발생 시켜 최적화하기 위함입니다.

--- src/components/RandomTemp.jsx
+++ src/components/RandomTemp.jsx
@@ -1,28 +1,34 @@
 import Slider from "rc-slider";
 import React from "react";
-import { Controller } from "react-hook-form";
+import { Controller, useWatch } from "react-hook-form";
 
-export default function RandomTemp({ product, control }) {
-  console.log(product);
-  const { id, initMax, initMin, max, min, name } = product;
+export default function RandomTemp({ idx, product, control }) {
+  const { initMax, initMin, name } = product;
+  const [currentMin, currentMax] = useWatch({
+    control,
+    name: `products[${idx}].temp`,
+  });
   return (
     <article>
       <p>{name}</p>
+      <p>{currentMin}</p>
       <Controller
-        name={name}
+        name={`products[${idx}].temp`}
         control={control}
         render={({ field }) => (
           <Slider
             {...field}
             range
-            onChange={(event) => field.onChange([event[0], event[1]])}
-            defaultValue={[Number(min), Number(max)]}
+            value={field.value}
+            onChange={(value) => field.onChange(value)}
+            defaultValue={field.value}
             min={Number(initMin)}
             max={Number(initMax)}
             allowCross={false}
           />
         )}
       />
+      <p>{currentMax}</p>
     </article>
   );
 }
--- src/components/RandomTempForm.jsx
+++ src/components/RandomTempForm.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { Controller, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import RandomTemp from "./RandomTemp";
 
 export default function RandomTempForm({ products }) {
-  const { register, handleSubmit, control } = useForm();
+  const { handleSubmit, control } = useForm({
+    mode: "onSubmit",
+    defaultValues: {
+      products: products.map(({ id, name, min, max }) => ({
+        id,
+        name,
+        temp: [Number(min), Number(max)],
+      })),
+    },
+  });
 
   const onSubmit = (formData) => {
     console.log(formData);
   };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <ul>
         {products.map((product, idx) => (
-          <li>
-            <RandomTemp product={product} control={control} />
+          <li key={idx}>
+            <RandomTemp idx={idx} product={product} control={control} />
           </li>
         ))}
       </ul>
